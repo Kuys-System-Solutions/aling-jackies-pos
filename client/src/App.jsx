68143ddafa82c4da1364274440b71b87d5ae0a36
Amiel Christian Mala-ay <amielchristian.malaay.cics@ust.edu.ph>
2025-05-02 15:34:13 +0800

    Add order history

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,20 @@
 // src/App.jsx
-import { useState, useEffect } from 'react';
 import './App.css';
 import KitchenView from './pages/KitchenView';
 import Index from './pages/Index';
 import OrderView from './pages/OrderView';
 import OrderHistory from './pages/OrderHistory';
-import StudioView from './pages/StudioView';  // Add this import
+import StudioView from './pages/StudioView'; // Add this import
 import { Routes, Route } from 'react-router';
-
-
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      <Route path="/" element={<Index />} /> {/* Done */}
       <Route path="/order-now/:mode" element={<OrderView />} />
       <Route path="/order-history" element={<OrderHistory />} />
-      <Route path="/kitchen" element={<KitchenView />} />
-      <Route path="/studio" element={<StudioView />} />  {/* Add this new route */}
+      <Route path="/kitchen" element={<KitchenView />} /> {/* Done */}
+      <Route path="/studio" element={<StudioView />} /> {/* Done */}
     </Routes>
   );
 }
